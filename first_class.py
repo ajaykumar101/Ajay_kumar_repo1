2f1c8db0bceac5a34cf5ca0b65012f9a016006f8
Ajay kumar <ajaykumaratukuri@gmail.com>
2025-05-08 13:45:23 -0700

    Ajay first commit for 1

--- first_class.py
+++ first_class.py
@@ -1,2 +1,14 @@
 print("Ajay_kumar_Atukuri")
 print("Gowtami_krishna_kumari_v")
+
+
+print("Ajay_kumar_Atukuri")
+print("Gowtami_krishna_kumari_v")
+
+
+print("Ajay_kumar_Atukuri")
+print("Gowtami_krishna_kumari_v")
+
+
+print("Ajay_kumar_Atukuri")
+print("Gowtami_krishna_kumari_v")
